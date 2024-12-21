7c3a980c96608fcd6070438792bc9ed4fa483813
ali17344 <elokctr96@gmail.com>
2024-12-21 17:20:58 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import InfoPage from "./InfoPage"
 function App() {
   const [data,setData] = useState([])
     const get_api = () => {
-          axios.get('https://rickandmortyapi.com/api/character ')
+          axios.get('https://rickandmortyapi.com/api/character?page=40 ')
           .then(({data}) => {setData(data.results)})
     }
   return (
